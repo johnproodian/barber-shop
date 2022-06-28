a1d2da0d8cada01fb7d8a2be6832d6e281e346d6
Jotas <jproodian721@gmail.com>
2022-06-28 11:24:48 -0500

    fix bug where couldn't type into inputs on login page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const Login = () => {
   const [formState, setFormState] = useState({ email: '', password: ''});
   const [login, { error }] = useMutation(LOGIN_USER);
 
+  // update state based on form input changes
   const handleChange = (event) => {
-    const { email, value } = event.target;
+    const { name, value } = event.target;
 
     setFormState({ 
       ...formState,
-      [email]: value
+      [name]: value
     });
   };
 
@@ -29,10 +31,10 @@ const Login = () => {
       console.error(e);
     }
 
-    setFormState({
-      email: '',
-      password: ''
-    })
+    // setFormState({
+    //   email: '',
+    //   password: ''
+    // })
   }
 
 
@@ -45,13 +47,13 @@ const Login = () => {
             <label className="login-label" for="email">
               Email:
             </label>
-            <input type="text" name="email" id="email" value={formState.email} onChange={handleChange} />
+            <input type="email" name="email" id="email" value={formState.email} onChange={handleChange} />
           </div>
           <div className="">
             <label className="login-label" for="password">
               Password:
             </label>
-            <input type="text" name="password" id="password" value={formState.password} onChange={handleChange} />
+            <input type="password" name="password" id="password" value={formState.password} onChange={handleChange} />
           </div>
           <div className="login-btn-div">
             <input className="login-btn" type="submit" value="Login" />
